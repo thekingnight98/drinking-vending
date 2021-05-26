ebd1bf3482a2c85138b5a8db2eb96adb3433095d
Arm Jakkit <thekingnight98@gmail.com>
2021-05-26 14:20:41 +0700

    change type create new machine

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -50,6 +50,7 @@
                             <v-col cols="12" sm="6" md="4">
                               <v-text-field
                                 v-model="editedItem.balance"
+                                type="Number"
                                 label="balance"
                               ></v-text-field>
                             </v-col>
@@ -68,35 +69,11 @@
                       </v-card-actions>
                     </v-card>
                   </v-dialog>
-                  <v-dialog v-model="dialogDelete" max-width="500px">
-                    <v-card>
-                      <v-card-title class="headline"
-                        >Are you sure you want to delete this
-                        item?</v-card-title
-                      >
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="closeDelete"
-                          >Cancel</v-btn
-                        >
-                        <v-btn
-                          color="blue darken-1"
-                          text
-                          @click="deleteItemConfirm"
-                          >OK</v-btn
-                        >
-                        <v-spacer></v-spacer>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
                 </v-toolbar>
               </template>
               <template v-slot:[`item.actions`]="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item, saveEdited)">
+                <v-icon small class="ml-3" @click="editItem(item, saveEdited)">
                   mdi-pencil
-                </v-icon>
-                <v-icon small @click="deleteItem(item)">
-                  mdi-delete
                 </v-icon>
               </template>
               <template v-slot:no-data>
@@ -224,18 +201,6 @@ export default {
       // console.log("editedItem = ", this.editedItem);
       this.dialog = true;
     },
-
-    deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
-
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
-      this.closeDelete();
-    },
-
     close() {
       this.dialog = false;
       this.$nextTick(() => {
